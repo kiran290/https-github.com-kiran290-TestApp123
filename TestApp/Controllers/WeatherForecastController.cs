562fff7eea562daffa80605d09e8f8364af31471
sudhakiran.ziraff <sudhakiran.ziraff@gmail.com>
2021-07-09 15:29:55 +0530

    Add project files.

---
+++ TestApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,72 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TestApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly DaprClient _daprClient;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, DaprClient daprClient)
+        {
+            _logger = logger;
+            _daprClient = daprClient;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("Welcome")]
+        public async Task<ActionResult> Welcome()//Crypto/Fiat
+        {
+            try
+            {
+                var forecasts = await _daprClient.InvokeMethodAsync<Result>(
+                    HttpMethod.Get,
+                    "testapp2",
+                    "weatherforecast/RestClient");
+
+
+                return Ok("Welcome to Member Module");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        
+
+    }
+
+    public class Result
+    {
+        public string data { get; set; }
+    }
+
+}
